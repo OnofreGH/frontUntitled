c1fac4827f70d43166c19553e463e566fd3b3340
Jesus Onofre <carlosonofreruiz16@gmail.com>
2025-02-21 11:55:07 -0500

    AREQUIPA LIST PRODUCTS

--- src/app/sections/products/listProducts/arequipa/arequipaList.tsx
+++ src/app/sections/products/listProducts/arequipa/arequipaList.tsx
@@ -1,10 +1,22 @@
 "use client";
+import Link from "next/link";
 import styles from "./arequipaList.module.scss";
+import products from "../../../../data/arequipa.product.json";
 
 export default function ArequipaList() {
   return (
     <main className={styles.main}>
       <h1 className={styles.title}>Productos en Arequipa</h1>
+      <ul className={styles.productList}>
+        {products.map((product) => (
+          <li key={product.id} className={styles.productItem}>
+            <Link href={`/products/arequipa/${product.id}`} className={styles.card}>
+              <h2>{product.nombre}</h2>
+              <p>Precio: {product.precio}</p>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
